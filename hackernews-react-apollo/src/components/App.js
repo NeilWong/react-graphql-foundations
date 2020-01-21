749f712bbded1a8e8b5b28957ab7b4107d491431
Neil <nwong1250@gmail.com>
2020-01-20 19:31:34 -0800

    added routing to new and create link components

--- hackernews-react-apollo/src/components/App.js
+++ hackernews-react-apollo/src/components/App.js
@@ -1,15 +1,21 @@
 import React from "react";
+import Header from "./Header";
 import LinkList from "./LinkList";
 import CreateLink from "./CreateLink";
 
 import "../styles/App.css";
+import { Route, Switch } from "react-router";
 
 function App() {
   return (
-    <div>
-      <p>Test</p>
-      <LinkList />
-      <CreateLink />
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
     </div>
   );
 }
